876dae71ccaac0699791ed5cccc71f09c2fe3250
Gimm17 <103266570+Gimm17@users.noreply.github.com>
2023-07-23 15:11:22 +0800

    script.js

--- static/js/script.js
+++ static/js/script.js
@@ -55,11 +55,20 @@ function initMap() {
 
     var rating = petshopRating[endNode];
     var distance = allDistances[endNode];
+    var orang = ratingCount[endNode];
 
     var popupContent =
       // "End Node: " +
       // menggunakanan fucnt diatas untuk menampilkan (End Node : Nama Petshop)
-      endNode + "<br>Rating: " + rating + "<br>Jarak: " + distance + " M";
+      endNode +
+      "<br>Rating: " +
+      rating +
+      " (" +
+      orang +
+      ")" +
+      "<br>Jarak: " +
+      distance +
+      " M";
     marker.setPopupContent(popupContent).openPopup();
   }
 
@@ -108,6 +117,238 @@ function initMap() {
     return { distances, previous };
   }
 
+  // MENAMBAHKAN VISUUALISASI RUTE
+  var currentRouteControl = null;
+
+  // Fungsi untuk menampilkan rute ke Jeslyn Petshop
+  function showJeslynPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.8869085, 119.8715397),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Faith Petshop
+  function showFaithPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.88314, 119.876132),
+        L.latLng(-0.8830524, 119.8758979),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Me-mow Petshop
+  function showMemowPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888722, 119.878981),
+        L.latLng(-0.890453, 119.882583),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke King Petshop & Grooming
+  function showKingPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.8814, 119.876297),
+        L.latLng(-0.880318, 119.872581),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Deyan Petshop
+  function showDeyanPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888774, 119.878691),
+        L.latLng(-0.896986, 119.877793),
+        L.latLng(-0.898181, 119.883622),
+        L.latLng(-0.8986076, 119.8816215),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Satellite Petshop
+  function showSatellitePetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888777, 119.87869),
+        L.latLng(-0.896984, 119.877878),
+        L.latLng(-0.901708, 119.883866),
+        L.latLng(-0.9064756, 119.8799512),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Aulia Home Petshop
+  function showAuliaPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888136, 119.87249),
+        L.latLng(-0.895241, 119.871203),
+        L.latLng(-0.90696, 119.870415),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Noah Petstore
+  function showNoahPetstoreRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.887844, 119.875296),
+        L.latLng(-0.888669, 119.878706),
+        L.latLng(-0.896904, 119.877691),
+        L.latLng(-0.897083, 119.883548),
+        L.latLng(-0.898106, 119.883444),
+        L.latLng(-0.898207, 119.883816),
+        L.latLng(-0.901375, 119.883818),
+        L.latLng(-0.906267, 119.884042),
+        L.latLng(-0.9062358, 119.8828971),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Kenzo Petshop
+  function showKenzoPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888136, 119.87249),
+        L.latLng(-0.895241, 119.871203),
+        L.latLng(-0.896289, 119.871329),
+        L.latLng(-0.898701, 119.872894),
+        L.latLng(-0.900845, 119.873727),
+        L.latLng(-0.905764, 119.875039),
+        L.latLng(-0.911566, 119.876228),
+        L.latLng(-0.9116352, 119.8784613),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menampilkan rute ke Zoey Petshop
+  function showZoeyPetshopRoute() {
+    clearCurrentRoute(); // Hapus rute sebelumnya
+    currentRouteControl = L.Routing.control({
+      waypoints: [
+        L.latLng(-0.8866987, 119.8755979),
+        L.latLng(-0.888136, 119.87249),
+        L.latLng(-0.895241, 119.871203),
+        L.latLng(-0.896289, 119.871329),
+        L.latLng(-0.898701, 119.872894),
+        L.latLng(-0.900845, 119.873727),
+        L.latLng(-0.905764, 119.875039),
+        L.latLng(-0.911227, 119.876066),
+        L.latLng(-0.9229054, 119.8644576),
+      ],
+      routeWhileDragging: true,
+      createMarker: function () {
+        return null; // Return null to prevent adding markers to the map
+      },
+    }).addTo(map);
+  }
+
+  // Fungsi untuk menghapus rute yang sedang ditampilkan dari peta
+  function clearCurrentRoute() {
+    if (currentRouteControl !== null) {
+      map.removeControl(currentRouteControl);
+      currentRouteControl = null;
+    }
+  }
+
+  // Event listener untuk tombol Confirm
+  document
+    .getElementById("confirmButton")
+    .addEventListener("click", function () {
+      var selectedPetshop = document.getElementById("petshopDropdown").value;
+
+      // Tampilkan rute berdasarkan pilihan pada dropdown
+      if (selectedPetshop === "jeslyn") {
+        showJeslynPetshopRoute();
+      } else if (selectedPetshop === "faith") {
+        showFaithPetshopRoute();
+      } else if (selectedPetshop === "memow") {
+        showMemowPetshopRoute();
+      } else if (selectedPetshop === "king") {
+        showKingPetshopRoute();
+      } else if (selectedPetshop === "deyan") {
+        showDeyanPetshopRoute();
+      } else if (selectedPetshop === "satellite") {
+        showSatellitePetshopRoute();
+      } else if (selectedPetshop === "aulia") {
+        showAuliaPetshopRoute();
+      } else if (selectedPetshop === "noah") {
+        showNoahPetstoreRoute();
+      } else if (selectedPetshop === "kenzo") {
+        showKenzoPetshopRoute();
+      } else if (selectedPetshop === "zoey") {
+        showZoeyPetshopRoute();
+      }
+    });
+
+  // Event listener untuk tombol Cancel
+  document
+    .getElementById("cancelButton")
+    .addEventListener("click", function () {
+      clearCurrentRoute(); // Hapus rute yang mungkin ditampilkan
+      document.getElementById("petshopDropdown").selectedIndex = 0; // Reset dropdown ke pilihan default
+    });
+
   // Definisikan graf dengan jarak antar node
   var graph = {
     STMIK: { "Jl. Soeprapto": 0 },
@@ -131,10 +372,18 @@ function initMap() {
     "Jl. Moh. Hatta": {
       "Jl. Jendral Ahmad Yani": 1000,
       "Jl. Mawar": 170,
+      "Jl. KH. Ahmad Dahlan": 114,
+    },
+    "Jl. KH. Ahmad Dahlan": {
+      "Jl. Moh. Hatta": 114,
+      "Jl. Sultan Hasanudin": 110,
+    },
+    "Jl. Sultan Hasanudin": {
+      "Jl. KH. Ahmad Dahlan": 110,
+      "Jl. Togian": 240,
     },
     "Jl. Mawar": {
       "Jl. Moh. Hatta": 170,
-      "Jl. Togian": 630,
       "Jl. Wolter Monginsidi": 380,
     },
     "Jl. Wolter Monginsidi": {
@@ -151,8 +400,16 @@ function initMap() {
       "Kenzo Petshop": 270,
     },
     "Jl. Togian": {
-      "Jl. Mawar": 630,
-      "Aulia Home Petshop": 1100,
+      "Jl. Patimura": 130,
+      "Jl. Sultan Hasanudin": 240,
+    },
+    "Jl. Patimura": {
+      "Jl. Togian": 130,
+      "Jl. Miangas": 750,
+    },
+    "Jl. Miangas": {
+      "Jl. Patimura": 750,
+      "Aulia Home Petshop": 290,
     },
     "Jl. Setia Budi": {
       "Jl. S. Parman": 400,
@@ -188,7 +445,7 @@ function initMap() {
       "Jl. G. Sidole": 260,
       "Deyan Petshop": 270,
     },
-    "Aulia Home Petshop": { "Jl. Togian": 1100 },
+    "Aulia Home Petshop": { "Jl. Miangas": 290 },
     "Zoey Petshop": { "Jl. I Gusti Ngurah Rai": 1500 },
     "Kenzo Petshop": { "Jl. Anoa": 270 },
     "Noah Petstore": { "Jl. Tj. Satu": 120 },
@@ -203,15 +460,29 @@ function initMap() {
   // Informasi rating untuk setiap petshop
   var petshopRating = {
     "Jeslyn Petshop": 4.5,
-    "Faith Petshop": 4.2,
-    "Me-mow Petshop": 4.8,
-    "King Petshop & Grooming": 4.0,
-    "Deyan Petshop": 4.1,
-    "Satellite Petshop": 4.6,
-    "Aulia Home Petshop": 4.3,
-    "Noah Petstore": 4.9,
-    "Kenzo Petshop": 4.7,
-    "Zoey Petshop": 4.4,
+    "Faith Petshop": 4.5,
+    "Me-mow Petshop": "-",
+    "King Petshop & Grooming": 4.6,
+    "Deyan Petshop": "-",
+    "Satellite Petshop": 4.5,
+    "Aulia Home Petshop": 5.0,
+    "Noah Petstore": 4.4,
+    "Kenzo Petshop": 5.0,
+    "Zoey Petshop": 5.0,
+  };
+
+  // Informasi jumlah orang yang memberikan rating untuk setiap petshop
+  var ratingCount = {
+    "Jeslyn Petshop": 6,
+    "Faith Petshop": 4,
+    "Me-mow Petshop": 0,
+    "King Petshop & Grooming": 164,
+    "Deyan Petshop": 0,
+    "Satellite Petshop": 2,
+    "Aulia Home Petshop": 8,
+    "Noah Petstore": 98,
+    "Kenzo Petshop": 4,
+    "Zoey Petshop": 2,
   };
 
   var startNode = "STMIK"; // Node awal
@@ -273,17 +544,35 @@ function initMap() {
     "M"
   );
 
-  // Menampilkan petshop dengan rating tertinggi
-  var petshopWithHighestRating = Object.keys(petshopRating).reduce(function (
-    a,
-    b
-  ) {
-    return petshopRating[a] > petshopRating[b] ? a : b;
-  });
+  // Fungsi untuk mencari petshop terpopuler berdasarkan skor
+  function findMostPopularPetshop() {
+    var highestScore = -1;
+    var mostPopularPetshop = "";
+
+    for (var petshop in petshopRating) {
+      var rating = petshopRating[petshop];
+      var count = ratingCount[petshop];
+
+      var score = rating * count;
+
+      if (score > highestScore) {
+        highestScore = score;
+        mostPopularPetshop = petshop;
+      }
+    }
+
+    return mostPopularPetshop;
+  }
+
+  // Memanggil fungsi untuk mencari petshop terpopuler
+  var mostPopularPetshop = findMostPopularPetshop();
+
+  // Menampilkan hasil
   console.log(
-    "Petshop Rating Tertinggi:",
-    petshopWithHighestRating + ", Rating:",
-    petshopRating[petshopWithHighestRating]
+    "Petshop Terpopuler:",
+    mostPopularPetshop + ", Rating:",
+    petshopRating[mostPopularPetshop] + ", Jumlah Pemberi Rating:",
+    ratingCount[mostPopularPetshop]
   );
 
   //Menampilkan informasi petshop terdekat saat button di html di klik
@@ -305,25 +594,35 @@ function initMap() {
   });
 
   //Menampilkan informasi petshop dengan rating tertinggi saat button di html di klik
-  var showHighestRatingRouteButton = document.getElementById(
-    "showHighestRatingRouteButton"
+  var showMostPopularPetshopButton = document.getElementById(
+    "showMostPopularPetshopButton"
   );
-  var highestRatingResult = document.getElementById("highestRatingResult");
+  var mostPopularPetshopResult = document.getElementById(
+    "mostPopularPetshopResult"
+  );
 
-  var isHighestRatingInfoVisible = false; // Menyimpan status visibilitas informasi petshop rating tertinggi
+  var isMostPopularPetshopInfoVisible = false; // Menyimpan status visibilitas informasi petshop populer
 
-  showHighestRatingRouteButton.addEventListener("click", function () {
-    // Toggle visibilitas informasi petshop rating tertinggi
-    if (isHighestRatingInfoVisible) {
-      highestRatingResult.value = ""; // Menghapus teks pada input
+  // Menambahkan event listener untuk meng-handle klik tombol
+  showMostPopularPetshopButton.addEventListener("click", function () {
+    // Toggle visibilitas informasi petshop paling populer
+    if (isMostPopularPetshopInfoVisible) {
+      mostPopularPetshopResult.value = ""; // Menghapus teks pada input
     } else {
-      highestRatingResult.value =
-        petshopWithHighestRating +
+      // Mencari petshop paling populer menggunakan fungsi yang telah dibuat sebelumnya
+      var mostPopularPetshop = findMostPopularPetshop();
+
+      // Menampilkan hasil informasi petshop paling populer pada input
+      mostPopularPetshopResult.value =
+        mostPopularPetshop +
         ", Rating: " +
-        petshopRating[petshopWithHighestRating];
+        petshopRating[mostPopularPetshop] +
+        ", Dari: " +
+        ratingCount[mostPopularPetshop] +
+        " Orang";
     }
 
-    isHighestRatingInfoVisible = !isHighestRatingInfoVisible; // Mengubah status visibilitas informasi
+    isMostPopularPetshopInfoVisible = !isMostPopularPetshopInfoVisible; // Mengubah status visibilitas informasi
   });
 
   // Tampilkan garis dari node awal (STMIK) ke petshop terdekat saat tombol diklik
@@ -359,43 +658,47 @@ function initMap() {
     }
   });
 
-  // Tampilkan garis dari node awal (STMIK) ke petshop dengan rating tertinggi saat tombol diklik
-  var showHighestRatingRouteButton = document.getElementById(
-    "showHighestRatingRouteButton"
+  // Tampilkan garis dari node awal (STMIK) ke petshop paling populer saat tombol diklik
+  var showMostPopularPetshopButton = document.getElementById(
+    "showMostPopularPetshopButton"
   );
-  var highestRatingRouteLine;
-  var highestRatingDistanceLabelMarker;
+  var mostPopularPetshopRouteLine;
+  var mostPopularPetshopDistanceLabelMarker;
 
-  showHighestRatingRouteButton.addEventListener("click", function () {
+  showMostPopularPetshopButton.addEventListener("click", function () {
     // Jika garis rute sudah ditampilkan, hapus garis dan label jarak
-    if (highestRatingRouteLine && highestRatingDistanceLabelMarker) {
-      map.removeLayer(highestRatingRouteLine);
-      map.removeLayer(highestRatingDistanceLabelMarker);
-      highestRatingRouteLine = null;
-      highestRatingDistanceLabelMarker = null;
+    if (mostPopularPetshopRouteLine && mostPopularPetshopDistanceLabelMarker) {
+      map.removeLayer(mostPopularPetshopRouteLine);
+      map.removeLayer(mostPopularPetshopDistanceLabelMarker);
+      mostPopularPetshopRouteLine = null;
+      mostPopularPetshopDistanceLabelMarker = null;
     } else {
-      // Tampilkan garis dari node awal (STMIK) ke petshop dengan rating tertinggi
-      var highestRatingPetshopCoordinates = getCoordinates(
-        petshopWithHighestRating
-      );
-      var highestRatingLineCoordinates = [
+      // Dapatkan petshop paling populer menggunakan fungsi yang telah dibuat sebelumnya
+      var mostPopularPetshop = findMostPopularPetshop();
+
+      // Tampilkan garis dari node awal (STMIK) ke petshop paling populer
+      var mostPopularPetshopCoordinates = getCoordinates(mostPopularPetshop);
+      var mostPopularPetshopLineCoordinates = [
         getCoordinates(startNode),
-        highestRatingPetshopCoordinates,
+        mostPopularPetshopCoordinates,
       ];
-      highestRatingRouteLine = L.polyline(highestRatingLineCoordinates, {
-        color: "blue",
-      }).addTo(map);
+      mostPopularPetshopRouteLine = L.polyline(
+        mostPopularPetshopLineCoordinates,
+        {
+          color: "blue",
+        }
+      ).addTo(map);
 
       // Tampilkan nilai jarak di atas garis
-      var highestRatingDistanceLabel = L.divIcon({
+      var mostPopularPetshopDistanceLabel = L.divIcon({
         className: "distance-label",
-        html: allDistances[petshopWithHighestRating].toString() + " M",
+        html: allDistances[mostPopularPetshop].toString() + " M",
         iconSize: [60, 20],
       });
-      highestRatingDistanceLabelMarker = L.marker(
-        highestRatingLineCoordinates[1],
+      mostPopularPetshopDistanceLabelMarker = L.marker(
+        mostPopularPetshopLineCoordinates[1],
         {
-          icon: highestRatingDistanceLabel,
+          icon: mostPopularPetshopDistanceLabel,
         }
       ).addTo(map);
     }
@@ -421,7 +724,7 @@ function getCoordinates(node) {
     STMIK: [-0.8866987, 119.8755979],
     "Jeslyn Petshop": [-0.8869085, 119.8715397],
     "Faith Petshop": [-0.8830524, 119.8758979],
-    "Me-mow Petshop": [-0.8904576, 119.8822754],
+    "Me-mow Petshop": [-0.890453, 119.882583],
     "King Petshop & Grooming": [-0.8803138, 119.8722627],
     "Deyan Petshop": [-0.8986076, 119.8816215],
     "Satellite Petshop": [-0.9064756, 119.8799512],
